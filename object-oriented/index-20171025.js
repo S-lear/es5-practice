0a68662461943a31451fb27512a65073bc158b74
songbo.zheng <saclear9310@gmail.com>
2017-10-27 09:04:35 +0800

    20171026-practice

--- object-oriented/index-20171025.js
+++ object-oriented/index-20171025.js
@@ -245,3 +245,72 @@ Range.prototype = hideProps({
   toString: function() {return '(' + this.from + '...' + this.to + ')';}
 })
 
+(function namespace() {
+  function properties() {
+    var names;
+    if(arguments.length == 0) {
+      names = Object.getOwnPropertyNames(this)
+    } else if(arguments.length == 1 && Array.isArray(arguments[0])) {
+      names = arguments[0]
+    } else {
+      names = Array.prototype.splice.call(arguments, 0)
+    }
+    return new Properties(this, names)
+  }
+
+  Object.defineProperty(Object.prototype, 'properties', {value: properties, enumerable: false, writable: true, configurable: true})
+
+  function Properties(o, names) {
+    this.o = o;
+    this.names = names;
+  }
+  Properties.prototype.hide = function() {
+    var o = this.o;
+    var hidden = {enumerable: false}
+
+    this.names.forEach(function(n) {
+      if(o.hasOwnProperty(n)) {
+        Object.defineProperty(o, n, hidden)
+      }
+    })
+    return this;
+  }
+  Properties.prototype.freeze = function() {
+    var o = this.o;
+    var frozen = {writable: false, configurable: false}
+    this.names.forEach(function(n){
+      if(o.hasOwnProperty(n)) {
+        Object.defineProperty(o, n, frozen)
+      }
+    })
+    return this;
+  }
+  Properties.prototype.descriptors = function() {
+    var o = this.o;
+    var desc = {};
+    this.names.forEach(function(n){
+      if(!o.hasOwnProperty(n)) return;
+      desc[n] = Object.getOwnPropertyDescriptor(o, n)
+    })
+    return desc;
+  }
+  Properties.prototype.toString = function() {
+    var o = this.o;
+    var lines = this.names.map(nameToString);
+    return '{\n ' + lines.join(',\n ') + '\n';
+    function nameToString(n) {
+      var s = '';
+      var desc = Object.getOwnPropertyDescriptor(o, n);
+      if(!desc) return 'nonexistent' + n + ': undefined';
+      if(!desc.configurable) s += 'permanent';
+      if((desc.get && !desc.set) || desc.writable) s += 'readonly';
+      if(!desc.enumerable) s += 'hidden';
+      if(desc.get || desc.set) s += 'accessor ' + n;
+      else s += n + ': ' + ((typeof desc.value === 'function') ? 'function' : desc.value);
+      return s;
+    }
+
+    Properties.prototype.properties().hide();
+    console.log(Object.prototype.properties)
+  }
+}())
